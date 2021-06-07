ddfdf259d369f1d71b88bc463e7c63efca6e072a
Narenddra M L <narenml2000@gmail.com>
2021-06-07 12:40:47 +0530

    Initial Commit

---
+++ src/components/Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+function Note() {
+  return (
+    <div className="note">
+      <h1> Javascript and React.js training</h1>
+      <p>
+        
+        i have attended the 7-days bootcamp which was being arranged by ShapeAI
+        and the classes were taken by Mr.Shaurya Sinha,covered all basics of
+        html,javascript and React.js and also explained the importance of web and react .
+      </p>
+    </div>
+  );
+}
+export default Note;
